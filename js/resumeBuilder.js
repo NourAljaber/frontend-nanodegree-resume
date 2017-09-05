db506036d7fcc2b75ccdfcf65c19c4fe64efab10
NourAljaber <noory738@gmail.com>
2017-09-05 06:57:04 +0300

    changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,31 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- console.log();
+
+ //var firstName = "Nour" ;
+ //var age = 25 ;
+ //console.log(firstName);
+
+ //$("#main").append(" NOUR AL JABER ");
+
+ //var awesomeThoughts = " I am Nour and i am awesome!" ;
+ //onsole.log(awesomeThoughts);
+
+
+// [string].replace([old],[new]);
+
+//var email = "noory738@gmail.com";
+//var newEmail=
+//email.replace("gmail", "hotmail");
+//console.log(email);
+//console.log(newEmail);
+
+//var awesomeThoughts = " I am Nour and i am awesome!" ;
+//var funThoughts =
+//awesomeThoughts.replace("awesome!", "Fun");
+//$("#main").append(funThoughts);
+
+
+
+var formattedName= HTMLheaderName.replace("%data%"," Nour Al Jaber ");
+$("#header").append(formattedName);
